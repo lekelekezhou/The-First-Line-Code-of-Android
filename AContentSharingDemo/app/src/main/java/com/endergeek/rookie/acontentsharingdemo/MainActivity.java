bab20d24f0182a947baabaec0488a2ecac1240bc
sennhviwang <sennhviwang@gmail.com>
2016-08-08 19:03:30 +0800

    UPDATE Content-text Sharing Demo, Image TBD, 2016/08/08

--- AContentSharingDemo/app/src/main/java/com/endergeek/rookie/acontentsharingdemo/MainActivity.java
+++ AContentSharingDemo/app/src/main/java/com/endergeek/rookie/acontentsharingdemo/MainActivity.java
@@ -1,13 +1,65 @@
 package com.endergeek.rookie.acontentsharingdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.endergeek.rookie.acontentsharingdemo.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Handle Text Sharing Section.
+    public void shareTextMessage(View view) {
+        // Called when user click the send button.
+        // An Intent can carry data types as key-value pairs called extras.
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        // use findViewById() to get the EditText element.
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        // Assign the text to a local message variable, and use the putExtra() method to add its text value to the intent.
+        String message = editText.getText().toString();
+
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
+    }
+
+    public void shareImage()
 }
